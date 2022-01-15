1f2bf2fd8bd713b42700169f2443565ee9ba7853
Sertac Özdemir <sertac.oezdemir@gmail.com>
2022-01-15 21:53:50 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <div>
+        <div className="header">
             <img src={logo} alt="logo" />
             <div className="whiteLine"></div>
             <div className="headerBar">
                 <nav>
                     <ul>
-                        <li><span>00</span><Link to="/">HOME</Link></li>
-                        <li><span>01</span><Link to="/destination">DESTINATION</Link></li>
-                        <li><span>02</span><Link to="/crew">CREW</Link></li>
-                        <li><span>03</span><Link to="/tech">TECHNOLOGY</Link></li>
+                        <li><span>00</span><Link className="link" to="/">HOME</Link></li>
+                        <li><span>01</span><Link className="link" to="/destination">DESTINATION</Link></li>
+                        <li><span>02</span><Link className="link" to="/crew">CREW</Link></li>
+                        <li><span>03</span><Link className="link" to="/tech">TECHNOLOGY</Link></li>
                     </ul>
                 </nav>
+            </div>
+            <div className="blurBar">
+                
             </div>
         </div>
     );
