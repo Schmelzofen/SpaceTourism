b89b9dff670c43790599fdc222eb35d64817698c
Sertac Özdemir <sertac.oezdemir@gmail.com>
2022-01-16 00:11:18 +0100

    technology not done yet

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -5,9 +5,9 @@ const Technology = () => {
         <section className="tech">
             <h1><span>03</span> SPACE LAUNCH 101</h1>
             <div className="techNav">
-                <Link to="/tech/launch"><div className="ball">1</div></Link>
-                <Link to="/tech/port"><div className="ball">2</div></Link>
-                <Link to="/tech/capsule"><div className="ball">3</div></Link>
+                <Link className="link2" to="/tech/launch"><div className="ball">1</div></Link>
+                <Link className="link2" to="/tech/port"><div className="ball">2</div></Link>
+                <Link className="link2" to="/tech/capsule"><div className="ball">3</div></Link>
             </div>
         </section>
     );
